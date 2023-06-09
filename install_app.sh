a891855e4b595c938c59a688a054a259aa3f0a43
linh.pham <linh.pham@setel.com>
2023-06-10 01:15:12 +0700

    fix: install order

--- install_app.sh
+++ install_app.sh
@@ -14,17 +14,17 @@ git clone https://github.com/kma-custom-nextcloud/tasks.git $FOLDER_APPS_PATH/ta
 cd $FOLDER_APPS_PATH/tasks
 make
 
+#wait
+git clone https://github.com/kma-custom-nextcloud/calendar.git $FOLDER_APPS_PATH/calendar
+cd $FOLDER_APPS_PATH/calendar
+make dev-setup
+make build-js
+
 nvm use 14.14.0
 
 #wait
 git clone https://github.com/kma-custom-nextcloud/spreed.git $FOLDER_APPS_PATH/spreed
 cd $FOLDER_APPS_PATH/spreed
-make dev-setup
-make build-js
-
-#wait
-git clone https://github.com/kma-custom-nextcloud/calendar.git $FOLDER_APPS_PATH/calendar
-cd $FOLDER_APPS_PATH/calendar
 make dev-setup
 make build-js
 
